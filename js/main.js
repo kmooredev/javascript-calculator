12936582cc2c5adc899c9f459d7c6a8c68168c2f
Korey Moore <kmoorefit5@gmail.com>
2023-05-29 20:03:56 -0500

    functioning calculator

---
+++ js/main.js
@@ -0,0 +1,95 @@
+const screen = document.querySelector('.screen');
+let runningTotal = 0;
+let previousOperator = null;
+let buffer = '0';
+
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  rerender();
+}
+
+function handleNumber(value) {
+  if (buffer === '0') {
+    buffer = value;
+  } else {
+    buffer += value;
+  }
+}
+
+function handleMath(value) {
+  if (buffer === 0){
+    // do nothing
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+  if(runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer)
+  }
+
+  previousOperator = value;
+  buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === '+'){
+    runningTotal += intBuffer;
+  } else if (previousOperator === '-'){
+    runningTotal -= intBuffer;
+  } else if (previousOperator === '÷'){
+    runningTotal /= intBuffer;
+  } else if (previousOperator === '×'){
+    runningTotal *= intBuffer;
+  }
+}
+
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case 'C':
+      buffer = '0';
+      break;
+    case '=':
+      if (previousOperator === null) {
+        // need two numbers to do math
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = '' + runningTotal;
+      runningTotal = 0;
+      break;
+    case '←':
+      if (buffer.length === 1){
+        buffer = '0';
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case '+':
+    case '×':
+    case '÷':
+    case '-':
+      handleMath(symbol);
+      break;
+  }
+}
+
+function init() {
+  document
+    .querySelector(".calc-buttons")
+    .addEventListener('click', function(event) {
+      buttonClick(event.target.innerText);
+    })
+}
+
+function rerender() {
+  screen.innerText = buffer;
+}
+
+init()
